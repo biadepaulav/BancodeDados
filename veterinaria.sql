6832984dd9a5723113406521eb024b9bb00b9055
biadepaulav <beatriz.de.paula290@gmail.com>
2026-07-30 12:28:30 -0300

    Add files via upload

---
+++ veterinaria.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 30/07/2026 às 17:27
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `veterinaria`
+--
+CREATE DATABASE IF NOT EXISTS `veterinaria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `veterinaria`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `animais`
+--
+
+CREATE TABLE `animais` (
+  `idanimal` int(11) NOT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `nomeanimal` varchar(255) NOT NULL,
+  `especie` varchar(255) NOT NULL,
+  `raca` varchar(100) NOT NULL,
+  `datanasc` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `animais`
+--
+
+INSERT INTO `animais` (`idanimal`, `idcliente`, `nomeanimal`, `especie`, `raca`, `datanasc`) VALUES
+(1, 1, 'valentina', 'cachorro', 'pinscher', '0000-00-00'),
+(2, 1, 'shakira', 'cachorro', 'pa', '0000-00-00'),
+(3, 1, 'hanna', 'cachorro', 'pa', '0000-00-00'),
+(4, 1, 'florzinha', 'tartaruga', 'tigredaagua', '0000-00-00'),
+(5, 2, 'toto', 'cachorro', 'rd', '0000-00-00'),
+(6, 3, 'ponyo', 'peixe', 'peixinho', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `atendimento`
+--
+
+CREATE TABLE `atendimento` (
+  `idate` int(11) NOT NULL,
+  `idanimal` int(11) DEFAULT NULL,
+  `idvet` int(11) DEFAULT NULL,
+  `dataatendimento` date NOT NULL,
+  `horaatendimento` time NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `valor` decimal(7,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idcliente` int(11) NOT NULL,
+  `nomecli` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cpf` varchar(100) NOT NULL,
+  `endereco` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`idcliente`, `nomecli`, `telefone`, `email`, `cpf`, `endereco`) VALUES
+(1, 'beatriz', '213891323', 'fsjhfjdshsjd@gmail.com', '123.123.123-12', 'rua hdj, num 3, irbeirao pires-sp'),
+(2, 'cintia', '89991323', 'fsjhfjdwershsjd@gmail.com', '123.123.923-12', 'rua urtv, num 3, irbeirao pires-sp'),
+(3, 'isabelly', '21098776323', 'fsfdgdshsjd@gmail.com', '123.122.123-12', 'rua hghg, num 3, irbeirao pires-sp');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `veterinario`
+--
+
+CREATE TABLE `veterinario` (
+  `idvet` int(11) NOT NULL,
+  `nomevet` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `especialidade` varchar(100) NOT NULL,
+  `crmv` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `veterinario`
+--
+
+INSERT INTO `veterinario` (`idvet`, `nomevet`, `telefone`, `especialidade`, `crmv`) VALUES
+(1, 'talita', '12345', 'animais oiticos', '12345'),
+(2, 'juliana', '67890', 'animais no geral', '5432'),
+(3, 'waldemir', '87654', 'animais grandes', '8762');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `animais`
+--
+ALTER TABLE `animais`
+  ADD PRIMARY KEY (`idanimal`),
+  ADD KEY `idcliente` (`idcliente`);
+
+--
+-- Índices de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD PRIMARY KEY (`idate`),
+  ADD KEY `idanimal` (`idanimal`),
+  ADD KEY `idvet` (`idvet`);
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idcliente`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- Índices de tabela `veterinario`
+--
+ALTER TABLE `veterinario`
+  ADD PRIMARY KEY (`idvet`),
+  ADD UNIQUE KEY `crmv` (`crmv`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `animais`
+--
+ALTER TABLE `animais`
+  MODIFY `idanimal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  MODIFY `idate` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `veterinario`
+--
+ALTER TABLE `veterinario`
+  MODIFY `idvet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `animais`
+--
+ALTER TABLE `animais`
+  ADD CONSTRAINT `animais_ibfk_1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`);
+
+--
+-- Restrições para tabelas `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`idanimal`) REFERENCES `animais` (`idanimal`),
+  ADD CONSTRAINT `atendimento_ibfk_2` FOREIGN KEY (`idvet`) REFERENCES `veterinario` (`idvet`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
